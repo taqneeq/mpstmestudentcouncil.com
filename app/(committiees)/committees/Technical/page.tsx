c287420cc8b3865d1741cd45f0aeddaa6d465d6d
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-09-15 10:35:27 +0530

    Added Pager Cards

--- app/(committiees)/committees/Technical/page.tsx
+++ app/(committiees)/committees/Technical/page.tsx
@@ -16,56 +16,25 @@ export default function Committees() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1">Technical Committees</h1>
           </div>
-          
 
-
-
-          <ul className="cards">
-
-          <li>
-    <a href="/committees/Technical/TRC" className="card">
-    <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
-
-      <div className="card__overlay">
-        <div className="card__header">
-          <div className="card__header-text">
-            <h3 className="card__title">TRC</h3>            
-          </div>
-        </div>
-        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+<section className="hero-section">
+  <div className="card-grid">
+    <a className="card" href="/committees/Technical/TRC">
+      <div className="card__background" ></div>
+      <div className="card__content">
+        <p className="card__category">TRC</p>
+        <h3 className="card__heading">Tech And Research Cell</h3>
       </div>
-    </a>      
-  </li>
-  <li>
-    <a href="" className="card">
-    <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
-
-      <div className="card__overlay">
-        <div className="card__header">
-          <div className="card__header-text">
-            <h3 className="card__title">TRC</h3>            
-          </div>
-        </div>
-        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>      
-  </li>
-  <li>
-    <a href="" className="card">
-    <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
-
-      <div className="card__overlay">
-        <div className="card__header">
-          <div className="card__header-text">
-            <h3 className="card__title">TRC</h3>            
-          </div>
-        </div>
-        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>      
-  </li>
-</ul>
+    </a>
+    
+  <div>
+    </div>
+    </div>
+</section>
 </div>
+
+
+
 </div>
 </section>
 
