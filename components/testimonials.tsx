92dd02365148aeb460b8477618b0d61133671a43
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-10-03 18:15:43 +0530

    revert

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -14,6 +14,7 @@ export default function Testimonials() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Our Team</h2>
+            <p className="text-xl text-gray-400">Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam ut venenatis tellus—in ornare.</p>
           </div>
 
           {/* Testimonials */}
